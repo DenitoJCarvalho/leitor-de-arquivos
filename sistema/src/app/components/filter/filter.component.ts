f850b6227e47142b536d39f1b8108429a725528a
Dennis <dennisjcarvalho@gmail.com>
2024-11-10 21:49:54 -0300

    feat(filter): ajustes no component

--- sistema/src/app/components/filter/filter.component.ts
+++ sistema/src/app/components/filter/filter.component.ts
@@ -39,4 +39,13 @@ export class FilterComponent implements ControlValueAccessor {
     this.isDisabled = isDisabled;
   }
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.onChange(file);
+    }
+  }
+
 }
