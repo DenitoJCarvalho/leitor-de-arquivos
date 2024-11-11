5138bc2eeaaca227063f36d583b9e9e2b74ac769
Dennis <dennisjcarvalho@gmail.com>
2024-11-11 18:11:30 -0300

    feat(component): criado componente input

---
+++ sistema/src/app/components/input/input.component.ts
@@ -0,0 +1,50 @@
+import { Component, forwardRef, input, model } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [
+    FormsModule, MatInputModule
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent implements ControlValueAccessor {
+
+  protected value = model<any>();
+  protected onChange: any = (value: any) => { };
+  protected onTouched: any = () => { };
+  protected isDisabled: boolean = false;
+
+  autoComplete = input<string>('off');
+  labelInput = input<string>();
+  placeholder = input<string>('');
+  tabIndex = input<string>();
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+
+}
--- sistema/src/app/pages/read-file/read-file.component.ts
+++ sistema/src/app/pages/read-file/read-file.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { FilterComponent } from '../../components/filter/filter.component';
+import { InputComponent } from '../../components/input/input.component';
 
 @Component({
   selector: 'app-read-file',
   standalone: true,
   imports: [
     MatDividerModule,
-    NavbarComponent, FilterComponent
+    InputComponent, NavbarComponent
   ],
   templateUrl: './read-file.component.html',
   styleUrl: './read-file.component.css'
