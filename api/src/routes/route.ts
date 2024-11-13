5975b1c9045dec5789227c6dc6b97eb3840a6648
Dennis <dennisjcarvalho@gmail.com>
2024-11-13 12:43:48 -0300

    build(api): removido a dependencia para mdb

--- api/src/routes/route.ts
+++ api/src/routes/route.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 
-import { mdbController } from '../controllers/mdb.controller';
 import { xlsxController } from '../controllers/xlsx.controller';
 import { csvController } from '../controllers/csv.controller';
 
-import { MdbService } from '../services/mdb.service';
 import { XlsxService } from '../services/xlsx.service';
 import { CsvService } from '../services/csv.service';
 import multer from 'multer';
@@ -12,15 +10,12 @@ import multer from 'multer';
 const router = Router();
 const upload = multer();
 
-const mdbService = new MdbService();
 const xlsxService = new XlsxService();
 const csvService = new CsvService();
 
-const mdb = new mdbController(mdbService);
 const xlsx = new xlsxController(xlsxService);
 const csv = new csvController(csvService);
 
-router.post('/read-file/mdb', mdb.readFile.bind(mdb));
 router.post('/read-file/xlsx', upload.single('file'), (req, res) => xlsx.readFile(req, res));
 router.post('/read-file/csv', upload.single('file'), (req, res) => csv.readFile(req, res));
 
