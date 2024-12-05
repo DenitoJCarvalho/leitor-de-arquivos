fdfffe56e85d493b9e75ed98091fc92b5f81a5c8
Dennis <dennisjcarvalho@gmail.com>
2024-12-05 19:08:11 -0300

    refactor: ajuste para ler arquivos csv e xlxs

--- sistema/src/app/pages/read-file/read-file.component.ts
+++ sistema/src/app/pages/read-file/read-file.component.ts
@@ -19,6 +19,7 @@ import { FileReaderService } from '../../services/file-reader.service';
 export class ReadFileComponent {
 
   private fileReadService: FileReaderService = inject(FileReaderService);
+  private validFilePattern = /\.(xlsx|csv)$/i;
 
   protected file?: File;
   protected result: any;
@@ -27,53 +28,53 @@ export class ReadFileComponent {
   protected label: string = `Selecione um arquivo do tipo .xlsx ou .csv`;
   protected headers: string[] = [];
 
-
   protected onFileSelected(file: File): void {
     this.file = file;
   }
 
-  private allowedFileTypes(type: string) {
-    const validFilePattern = /\.(xlsx|csv)$/i;
-    return validFilePattern.test(type);
+  private allowedFileTypes(type: string): boolean {
+    return this.validFilePattern.test(type);
   }
 
-  private validateFile(): boolean {
+  private validateFile() {
 
     if (!this.allowedFileTypes(this.file?.name as string)) {
       console.error(`Erro: Arquivo inválido! Por favor informe um arquivo válido.`);
       this.error = `Erro: Arquivo inválido! Por favor informe um arquivo válido.`;
       return false;
+    } else {
+      return this.file;
     }
-
-    if (!this.file) {
-      console.error(`Erro:  Arquivo vázio.`);
-      this.error = `Erro: Arquivo vazio.`;
-      return false;
-    }
-
-    this.error = null;
-    return true;
-
   }
 
   protected processFile(): void {
     if (this.validateFile()) {
-      const fileObservable = this.file?.name.endsWith('.xlsx')
-        ? this.fileReadService.readFileXlsx(this.file as File)
-        : this.fileReadService.readFileCsv(this.file as File)
 
-      fileObservable.subscribe({
-        next: (res) => {
-          console.log(Object.keys(res.data[0]));
-          console.log(res);
-          this.headers = Object.keys(res.data[0]);
+      if (this.file?.name.match(/.xlsx/)) {
+        this.fileReadService.readFileXlsx(this.file).subscribe({
+          next: (res) => {
+            this.headers = Object.keys(res.data[0]);
+            this.result = Object.values(res.data);
+          },
+          error: (e) => {
+            console.error(e);
+            this.error = e;
+          }
+        });
+      }
 
-        },
-        error: (e) => {
-          console.error(e);
-          this.error = e;
-        }
-      });
+      if (this.file?.name.match(/.csv/)) {
+        this.fileReadService.readFileCsv(this.file).subscribe({
+          next: (res) => {
+            this.headers = Object.keys(res.data[0]);
+            this.result = Object.values(res.data);
+          },
+          error: (e) => {
+            console.error(e);
+            this.error = e;
+          }
+        });
+      }
     }
 
     setTimeout(() => this.error = null, 5000);
